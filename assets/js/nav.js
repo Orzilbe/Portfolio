bc47e4664314c47ccbc7c1c264036d29f6052c0b
Or Zilberberg <orzilbe@gmail.com>
2026-04-08 16:02:15 +0300

    Update footer navigation links to use GitHub Pages

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     
     if (footerNav) {
         footerNav.innerHTML = `
-            <a href="/index.html">Home</a>
-            <a href="/public/about.html">About Me</a>
-            <a href="/public/projects.html">Projects</a>
-            <a href="/public/contact.html">Contact</a>
+            <a href="https://orzilbe.github.io/Portfolio">Home</a>
+            <a href="https://orzilbe.github.io/Portfolio/public/about.html">About Me</a>
+            <a href="https://orzilbe.github.io/Portfolio/public/projects.html">Projects</a>
+            <a href="https://orzilbe.github.io/Portfolio/public/contact.html">Contact</a>
             <a href="https://www.linkedin.com/in/orzilberberg/" target="_blank">LinkedIn</a>
         `;
     }
